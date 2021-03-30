0fcc88ac3dd2e9a4314ab11a13ae766dd02f492b
Marco64 <c14180017@john.petra.ac.id>
2021-03-30 20:09:17 +0700

    Tes1mb

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
+//Variabel Penampung
+  DataNama : string
+  DataNrp : string
+  DataEmail : string
+  DataTelp : string
+
+  //Variabel kiriman
+  Nama : string
+  Nrp : string
+  Telp : string
+  Em : string
+
+  BSubmit()
+  {
+    this.DataNama = this.Nama
+    this.DataNrp = this.Nrp
+    this.DataEmail= this.Em
+    this.DataTelp = this.Telp
+  }
+
 
 }
